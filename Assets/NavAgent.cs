f560c556306bf58fd5c790b16a77edf943f941eb
Amperess <aarawson@gmail.com>
2017-05-29 12:25:22 -0400

    Update 1
    Potential Bug Fix for navmesh

--- Assets/NavAgent.cs
+++ Assets/NavAgent.cs
@@ -5,34 +5,13 @@ using UnityEngine.AI;
 
 public class NavAgent : MonoBehaviour {
     public Transform target;
-    Transform self;
     NavMeshAgent agent;
-    Animator a;
-    float v;
-    Vector3 cp, pp;
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
-        a = GetComponent<Animator>();
-        self = GetComponent<Transform>();
-        cp = self.position;
-        pp = self.position;
     }
 	
 	// Update is called once per frame
 	void Update () {
         agent.SetDestination(target.position);
-        /*
-        cp = self.position;
-        v = (cp - pp).magnitude / Time.deltaTime;
-        if (Mathf.Abs(0-v) > 0.1)
-        {
-            a.SetBool("isMoving", true);
-        }
-        else
-        {
-            a.SetBool("isMoving", false);
-        }
-        pp = self.position;
-        */
     }
 }
